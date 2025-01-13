4958af0e3a94e6e0f2d62b7cd546cd2ebc7abfe1
Sujarta Jaya <sujarta.jaya@outlook.co.id>
2025-01-13 14:35:04 +0800

    my radius

--- laravel/src/resources/views/user/login.blade.php
+++ laravel/src/resources/views/user/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.app')
     @section('content')
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 mt-10">
-            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 justify-center items-center">
+            <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 justify-center items-center overflow-hidden" style="background-image: url('bg.jpg');">
                 <div class="flex items-center justify-center">
-                    <img src="/logo.png" alt="Logo" width="30%" height="30%"/>
+                    <img src="/medialink-logo.png" alt="Logo" width="30%" height="30%"/>
                 </div>
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     
