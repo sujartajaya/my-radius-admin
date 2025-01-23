3e886c8ec2cbd569dfe6ab00cad1ba443c2415a1
Sujarta Jaya <sujarta.jaya@outlook.co.id>
2025-01-23 11:55:56 +0800

    my radius

--- laravel/src/resources/views/hotspot/userguest.blade.php
+++ laravel/src/resources/views/hotspot/userguest.blade.php
@@ -44,13 +44,33 @@
                                     <td class="border border-gray-300 px-4 py-2 text-sm text-gray-700">{{ $email->username }}</td>
                                     <td class="border border-gray-300 px-4 py-2 text-sm text-gray-700">
                                         <div class="flex space-x-2">
+                                            <div class="relative group">
+                                            <button onClick="alert('Profile')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
+                                            <div class="absolute mt-1 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-sm rounded-lg px-3 py-2 opacity-0 group-hover:opacity-100 transition-opacity">
+                                                Profile {{$email->username}}
+                                            </div>
+                                            </div>
+                                            <button data-tooltip-target="tooltip-light" data-tooltip-style="light">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                                                <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                                                </svg>
+                                            </button>
+                                            <div id="tooltip-light" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
+                                                Edit data
+                                                <div class="tooltip-arrow" data-popper-arrow></div>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
-                    <div class="w-full text-sm text-left rtl:text-right text-gray-500 mt-2">
+                    <div class="w-full text-sm text-left rtl:text-right text-gray-500 mt-4">
                         {{$emailusers->withQueryString()->links('pagination::tailwind')}}
                     </div>
                     </div>
